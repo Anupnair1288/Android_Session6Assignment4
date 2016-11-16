07cc6b9a11d091456996605d8ef86447b01e734c
Anup S Nair <anupnair1288@gmail.com>
2016-11-17 01:14:50 +0530

    Implement context menu
    Implement context menu having two options call and sms. Contains a
    custom list having two text views in row. One text view for name and
    another for phone number. Implicit intent action is used for call and
    sms.

---
+++ ContextMenu/app/src/main/java/com/android/anup/contextmenu/Pojo.java
@@ -0,0 +1,21 @@
+package com.android.anup.contextmenu;
+
+class Pojo {
+    private String name, number;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    String getNumber() {
+        return number;
+    }
+
+    void setNumber(String number) {
+        this.number = number;
+    }
+}
